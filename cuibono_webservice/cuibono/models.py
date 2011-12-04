7d69dafaeb3443b6e64858b188726519fd316f87
Pam Selle <pamela.selle@gmail.com>
2011-12-04 11:29:37 -0500

    Editing models again, making Ads have many articles. Next, return an ad's articles in the API

--- cuibono_webservice/cuibono/models.py
+++ cuibono_webservice/cuibono/models.py
@@ -8,19 +8,23 @@ class Tag(models.Model):
     value = models.CharField(max_length=200)
     type = models.ForeignKey(TagType)
 
-class Ad(models.Model):
-    title = models.CharField(max_length=200)
-    transcript = models.CharField(max_length=500)
-    tag = models.ManyToManyField(Tag, blank=True)
-    audio_hash = models.CharField(max_length=5000)
-
-class Quotation(models.Model):
-    article = models.ForeignKey('Article')
-
 class Article(models.Model):
     title = models.CharField(max_length=200)
     raw_content = models.CharField(max_length=10000)
     source = models.CharField(max_length=500)
     pub_date = models.DateTimeField('date published')
     url = models.CharField(max_length=500)
+    def __unicode__(self):
+        return self.title
 
+class Quotation(models.Model):
+    article = models.ForeignKey('Article')
+    
+class Ad(models.Model):
+    title = models.CharField(max_length=200)
+    transcript = models.CharField(max_length=500, blank=True)
+    tag = models.ManyToManyField(Tag, blank=True)
+    audio_hash = models.CharField(max_length=5000)
+    articles = models.ManyToManyField(Article, blank=True)
+    def __unicode__(self):
+        return self.title
