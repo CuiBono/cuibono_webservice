57130563395e4a62b0a96ebf798069f2991cba50
Jim <ludflu@gmail.com>
2011-12-10 11:44:42 -0500

    ad url mapping for api

---
+++ cuibono_webservice/api/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import patterns, include, url
+from piston.resource import Resource
+from piston.authentication import HttpBasicAuthentication
+from api.handlers import AdHandler
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+
+ad_resource = Resource(handler=AdHandler)
+
+urlpatterns = patterns('',
+    
+    url(r'^ad/(?P<the_hash>[^/]+)/$', ad_resource), # Where is ad_resource?
+    
+)
