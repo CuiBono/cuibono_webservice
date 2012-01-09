4e6aa7ec712ce235fc146e83a78914c4a9c94e13
Bob Lannon <blannon@gmail.com>
2012-01-09 01:15:46 -0500

    django complained about the form filefield having a kwarg.  very confusing, this.

--- cuibono_webservice/submit/forms.py
+++ cuibono_webservice/submit/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 class SubmitAdForm(forms.Form):
     ad_title = forms.CharField()
-    ad_file = forms.FileField(upload_to="ad_media")
+    ad_file = forms.FileField()
     ad_funder = forms.CharField()
     ad_tags = forms.CharField()
     article_url = forms.URLField()
