7b6bc37af6da035907f6126ddc864541be42a277
Bob Lannon <blannon@gmail.com>
2012-01-22 14:20:44 -0500

    fixed syntax error in settings.py

--- settings.py
+++ settings.py
@@ -113,7 +113,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/var/www/cuibono_webservice/templates'),
+    '/var/www/cuibono_webservice/templates',
 )
 
 INSTALLED_APPS = (
