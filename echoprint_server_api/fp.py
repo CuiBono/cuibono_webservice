48670079a7eb1277104219cf3fd6e4d92954692f
Bob Lannon <blannon@gmail.com>
2012-02-13 22:09:45 -0500

    still trying to get fp perfect

--- echoprint_server_api/fp.py
+++ echoprint_server_api/fp.py
@@ -248,12 +248,12 @@ def best_match_for_query(code_string, elbow=10, local=False):
     trackid = actual_score_top_track_id.split("-")[0]
     meta = metadata_for_track_id(trackid, local=local)
     
-    if actual_score_top_score < code_len * 0.1:
-        return Response(Response.MULTIPLE_BAD_HISTOGRAM_MATCH, qtime = response.header["QTime"], tic=tic)
-    else:
+    #if actual_score_top_score < code_len * 0.1:
+        #return Response(Response.MULTIPLE_BAD_HISTOGRAM_MATCH, qtime = response.header["QTime"], tic=tic)
+    #else:
         # If the actual score went down it still could be close enough, so check for that
         #if actual_score_top_score > (original_scores[actual_score_top_track_id] / 2): 
-    #if True: 
+    if True: 
             # CUIBONO: THIS IS HURTING OUR RESULTS.
             #(actual_score_top_score - actual_score_2nd_score) >= (actual_score_top_score / 2):  # for examples [10,4], 10-4 = 6, which >= 5, so OK
         #return Response(Response.MULTIPLE_GOOD_MATCH_HISTOGRAM_DECREASED, TRID=trackid, score=actual_score_top_score, qtime=response.header["QTime"], tic=tic, metadata=meta)
