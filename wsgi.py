78c8df55e84dfa8855e955eae3d240f2e02145e8
Bob Lannon <blannon@gmail.com>
2012-06-08 13:33:25 -0400

    testing hook trick

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+#added just to test hook trick
+
 sys.path.append('/var/www/')
 #sys.path.append('/var/www/cuibono_webservice/')
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
