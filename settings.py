25c5e79125152d2d53b46582deb2347d3942cff2
Bob Lannon <blannon@gmail.com>
2012-06-08 13:58:27 -0400

    Adding south support

--- settings.py
+++ settings.py
@@ -131,6 +131,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable the admin:
     # Uncomment the next line to enable admin documentation:
      'django.contrib.admindocs',
+     'south'
 )
 
 # A sample logging configuration. The only tangible logging
