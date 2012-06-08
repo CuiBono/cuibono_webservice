0a3d91a14c474cb7a3ea2a114aa2ebd7a8132edc
Bob Lannon <blannon@gmail.com>
2012-06-08 13:14:38 -0400

    updated settings.py for new configuration

--- settings.py
+++ settings.py
@@ -63,7 +63,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/var/www/static/'
+STATIC_ROOT = '/srv/www/uwsgi/app/cuibono_webservice/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
